5c22668b6e24797c61f4bdd7d4941471fc96600d
aadishiv <aadishiv@outlook.com>
2024-12-04 17:20:23 -0500

    tf 2.x compatability

--- logger.py
+++ logger.py
@@ -2,16 +2,19 @@ import tensorflow as tf
 import os
 import shutil
 
-
 class Logger(object):
     def __init__(self, log_dir, suffix=None):
         """Create a summary writer logging to log_dir."""
-        self.writer = tf.compat.v1.summary.FileWriter(log_dir, filename_suffix=suffix)
+        if suffix:
+            log_dir = os.path.join(log_dir, suffix)
+        self.writer = tf.summary.create_file_writer(log_dir)
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step)
+            self.writer.flush()
+
 
 
 class ModelLogger(object):
